42937476fcf2baa1cc0dfe46eecb0800bb3cc069
Richard Hirata <hirata.rich@gmail.com>
2019-06-21 17:17:53 +0900

    Add files via upload

--- dht11_example.py
+++ dht11_example.py
@@ -1,21 +1,16 @@
 import RPi.GPIO as GPIO
-import dht11
-import time
-import datetime
-
+import datetime,time,RPi,dht11
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-
-# read data using pin 14
-instance = dht11.DHT11(pin=14)
+instance =dht11. DHT11(pin=14)
 
 while True:
     result = instance.read()
     if result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
-        print("Temperature: %d C" % result.temperature)
-        print("Humidity: %d %%" % result.humidity)
+        print("時刻: " + str(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')),end="")
+        print(" 温度: %d C " % result.temperature,end="")
+        print("湿度: %d %%" % result.humidity)
+    time.sleep(2)
 
-    time.sleep(1)
